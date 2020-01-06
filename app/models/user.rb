14dc653c594ae6bb928704d702d46d4e64d8d8af
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-01-06 19:11:13 -0300

    Change email regex constant to variable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@
 
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  VALID_EMAIL_REGEX.freeze
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  valid_email_regex = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: valid_email_regex }, uniqueness: true
   has_many :posts
   has_many :comments
 end
