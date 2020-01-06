cb9fe4d42a72fd87df7a95370608a8781557bf0a
Selma Datagirl <datagirlcmr@gmail.com>
2020-01-06 15:28:08 -0600

    Generated the post model and designed the associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200106193040) do
+ActiveRecord::Schema.define(version: 20200106212143) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
